2dee8a4650af04045576982db37d7f1bda96b85e
joao pedro <joaocarrasco2908@gmail.com>
2025-08-15 11:06:19 -0300

    Adicionar arquivos de projeto.

---
+++ Piaget/Data/PiagetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Piaget.Models;
+
+namespace Piaget.Data
+{
+    public class PiagetContext : DbContext
+    {
+        public PiagetContext (DbContextOptions<PiagetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Piaget.Models.Aluno> Aluno { get; set; } = default!;
+    }
+}
---
+++ Piaget/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Piaget.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
